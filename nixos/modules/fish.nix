13dcaa366f4fbf6b562f8a8ab7244fd34700d213
eiiko6 <mxstotolatomate@gmail.com>
2025-02-17 08:44:17 +0100

    cleaned up fish config

--- nixos/modules/fish.nix
+++ nixos/modules/fish.nix
@@ -8,7 +8,6 @@
       ".." = "cd ..";
       "..." = "cd ../../";
       grep = "grep --color=auto";
-      tt = "tt -n 10 -theme custom";
       ff = "fastfetch -c ~/.config/fastfetch/image.jsonc";
       ffs = "fastfetch -c ~/.config/fastfetch/small.jsonc --logo-type small";
       fff = "fastfetch -c ~/.config/fastfetch/full.jsonc";
@@ -16,21 +15,12 @@
       rb = "shutdown -r 0";
       vim = "nvim";
       svim = "sudo nvim";
-      p = "sudo pacman";
       ls = "exa -1lhmU --group-directories-first --no-permissions --no-user --icons --color always --sort date -r --time-style iso";
       lsa = "exa -1alhmUF --group-directories-first --no-permissions --no-user --icons --color always --sort date -r --time-style iso";
       cls = "c && lsa";
-      lls = "/bin/ls";
       c = "clear";
-      dim = "echo 70 | sudo tee /sys/class/backlight/intel_backlight/brightness";
-      clock = "tty-clock -s -C 5";
-      led = "echo off | sudo tee /sys/class/sound/ctl-led/mic/mode";
-      commits = "~/Desktop/git-repos/commits/commits.sh";
       cm = "chezmoi";
-      dcolors = "Desktop/scripts/display-colors.fish";
       cmcd = "cd /home/strawberries/.local/share/chezmoi/";
-      deb = "sudo lxc-start -n debian -f /var/lib/lxc/debian/config -d; sudo lxc-attach -n debian -- login";
-      t = "tmux";
       ta = "tmux attach";
       theme = "~/.config/scripts/palette/change-wallpaper.sh";
     };
@@ -38,10 +28,6 @@
     interactiveShellInit = ''
       function palette
           ~/.config/scripts/palette/change-wallpaper.sh $argv
-      end
-
-      function shufbg
-          fish -c 'source ~/.config/fish/functions/palette.fish; set wallpaper (lls ~/Pictures/Wallpapers/rin/ | shuf -n 1 | sed s/.png\$//); palette "rin/$wallpaper"'
       end
 
       if status is-interactive
@@ -56,9 +42,6 @@
 
       # Set the prompt
       starship init fish | source
-
-      # Add PATH
-      set PATH $PATH /home/strawberries/.local/bin
 
       # Bindings
       bind \b 'commandline -r ""'
